31fc5f8c1f5fb441412c15e18a8707358dada0fd
alejasl2611 <alejasl2611@gmail.com>
2021-09-22 11:09:28 -0500

    Paciente incrementado

---
+++ HospiEnCasa.App.Persistencia/AppRepositorios/IRepositorioPaciente.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using HospiEnCasa.App.Dominio;
+
+namespace HospiEnCasa.App.Persistencia
+{
+    public interface IRepositorioPaciente
+    {
+        Paciente AddPaciente (Paciente paciente);
+    }
+}
